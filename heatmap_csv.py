b11d5b52ce9729b4a54b66148363394cdb28e82b
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-04 14:21:28 +0200

    also display with wrong type

--- heatmap_csv.py
+++ heatmap_csv.py
@@ -7,7 +7,19 @@ parser.add_argument('--csv')
 parser.add_argument('--sep', default=',')
 args = parser.parse_args()
 
-arr = np.loadtxt(args.csv,
-                 delimiter=args.sep, dtype=str).astype("float32")
+def isNumeric(s):
+   s = s.strip()
+   try:
+      s = float(s)
+      return True
+   except:
+      return False
+
+arr = np.genfromtxt(args.csv,delimiter=args.sep, dtype='str')
+#t = np.where(canBeNumber(arr),arr, 0)
+arr.flat = [ float(e) if isNumeric(e) else np.nan for e in arr.flat]
+
+arr = arr.astype(np.float32)
+
 plt.imshow(arr)
 plt.show()
