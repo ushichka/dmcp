4bfb28cc14f5f7aa39915e57d4ccda5beefdbaea
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-11 10:40:40 +0200

    empty project

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setup
+To start working on this repo, cd in to it and exec ``` pip install -e . ```
+
+For ipython use 
+```
+%load_ext autoreload
+%autoreload 2
+```
+at the beginning to automatically reload changes in src without reinstalling.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
-    license='MIT',
+    description='local project',
+    author='Julian Jandeleit',
+    license='None',
 )
