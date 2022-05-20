e4a7830323a6c0c2cd89e4f409276f1f81cc076b
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-20 14:45:31 +0200

    clean repo

--- src/dmcpworkflow/capture_depth.py
+++ src/dmcpworkflow/capture_depth.py
@@ -1,80 +1,19 @@
 from PIL import Image
-from .h_backproject_mesh import capture_depth
+from .h_backproject_mesh import get_interactive_camera, capture_scene
 import math
 import pyvista as pv
 import argparse
 import numpy as np
 import scipy.misc
 import scipy.io
+import scipy.linalg as la
 
-def generate_depth_map(mesh):
-
-    plotter = pv.Plotter(off_screen=False, notebook=False)
-    actor = plotter.add_mesh(mesh, color="grey")
-    def clicked(event):
-        up = plotter.camera.GetViewUp()
-        forward = plotter.camera.GetDirectionOfProjection()
-        right = np.cross(forward, up)
-        right = right / np.linalg.norm(right)
-        T = plotter.camera.GetPosition()
-
-    plotter.track_click_position(callback=clicked, side='left', viewport=True)
-
-    w, h = plotter.window_size
-
-    plotter.show()
-
-
-    T = plotter.camera.GetPosition()
-    T = [T[0], T[1], T[2]]
-
-    up = plotter.camera.GetViewUp()
-    forward = plotter.camera.GetDirectionOfProjection()
-    right = np.cross(forward, up)
-    right = right / np.linalg.norm(right)
-    u = up
-    f = forward
-    r = right
-    R = np.array([[r[0], -u[0], f[0]],
-                [r[1], -u[1], f[1]],
-                [r[2], -u[2], f[2]]])
-
-    C = np.hstack((R, np.array([T]).T))
-    C = np.vstack((C, [0, 0, 0, 1]))
-
-
-    E = np.linalg.inv(C)
-    E = E[0:3, 0:4]
-
-    K = np.array([[526, 0, 320], [0, 526, 256], [0, 0, 1]])
-
-    wcx, wcy = plotter.camera.GetWindowCenter()
-
-    cx = w * wcx/-2+float(w)/2
-    cy = h * wcy/-2+float(h)/2
-
-    # convert the focal length to view angle and set it
-    view_angle = plotter.camera.GetViewAngle()
-
-    # it was (2* ,math.tan...) but 1*... seems to work
-    f_x = -w / (1 * math.tan(view_angle/2.0))
-    f_y = -h / (1 * math.tan(view_angle/2.0))
-
-    K = np.array([[f_x, 0, cx],
-                [0, f_y, cy],
-                [0, 0,  1]])
-
-
-    P = np.matmul(K, E)
-
-    # capture photo
-    n_rows = h
-    n_cols = w
-
-
-    depth_map = capture_depth(mesh, P, K, n_rows, n_cols)
-
-    return depth_map, K, P
+def generate_depth_map(mesh,imK):
+    K, pose, shape = get_interactive_camera(mesh,imK)
+    img, depth = capture_scene(mesh, imK, pose[:3,:3], pose[:3,-1], shape[1], shape[0])
+    extr = la.inv(np.vstack((pose,[0,0,0,1])))[:3,:]
+    P = K @ extr
+    return depth, K, P
 
 if __name__ == "__main__":
     # cli args
