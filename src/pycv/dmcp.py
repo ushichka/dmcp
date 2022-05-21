7980cc203d34d642217f12a47c3c7145ba794eb7
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-21 15:01:38 +0200

    add scale factor

--- src/pycv/dmcp.py
+++ src/pycv/dmcp.py
@@ -1,6 +1,7 @@
 from turtle import distance
 import numpy as np
 import scipy.linalg as la
+from sklearn.preprocessing import scale
 from src.pycv.perspective import calibrate_dlt, solve_PnP, horn_affine_transformation
 
 def dm_to_world(dm: np.ndarray, dmK: np.ndarray, dmP: np.ndarray, dmPts: np.ndarray):
@@ -49,6 +50,22 @@ def dmcp(K_native: np.ndarray,P_native: np.ndarray, box_native_x_native: np.ndar
     if box_world.shape[1] != 3:
         raise Exception("box_world must have 3 columns")
 
+    
+    def estimate_scaling(K1, P1, K2, P2):
+        E1 = np.vstack((la.inv(K1) @ P1, [0, 0, 0, 1]))
+        E2 = np.vstack((la.inv(K2) @ P2, [0, 0, 0, 1]))
+
+        # ref: https://math.stackexchange.com/questions/237369/given-this-transformation-matrix-how-do-i-decompose-it-into-translation-rotati/417813
+        sv_1 = [la.norm(E1[0:3, 0]), la.norm(E1[0:3, 1]), la.norm(E1[0:3, 2])] # vector of each norm of column in rotation matrix
+        sv_2 = [la.norm(E2[0:3, 0]), la.norm(E2[0:3, 1]), la.norm(E2[0:3, 2])]
+
+        sv_1= np.array(sv_1)
+        sv_2 = np.array(sv_2)
+
+        scale_factor = la.norm(sv_2) / la.norm(sv_1)
+        return scale_factor
+
+
 
 
 
@@ -64,12 +81,17 @@ def dmcp(K_native: np.ndarray,P_native: np.ndarray, box_native_x_native: np.ndar
     print(pose_matrix)
     pose_matrix_hat = np.vstack((pose_matrix, [0,0,0,1]))
     extrinsic_matrix_world = la.inv(pose_matrix_hat)[:3,:]
+    P = K_native @ extrinsic_matrix_world
 
     # DMCP Step 2 compute registering transform
     # DMCP Step 2.1 transform world points into camera space
+    
+    scale_factor = estimate_scaling(K_native,P,K_native, P_native)
+    print(f"scale factor {scale_factor}")
+
     box_world_hat = np.hstack((box_world, np.ones((box_world.shape[0],1))))
 
-    box_world_camera = np.matmul(extrinsic_matrix_world, box_world_hat.T).T
+    box_world_camera = np.matmul(extrinsic_matrix_world, box_world_hat.T).T * scale_factor
 
 
     # DMCP Step 2.2 transform camera points into native space
