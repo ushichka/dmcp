78bf9106d75c078b02b155e9818454b5b3a248c5
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-28 18:26:21 +0200

    improve plots

--- tools/heatmap_csv.py
+++ tools/heatmap_csv.py
@@ -5,6 +5,7 @@ import colorcet as cc
 parser = argparse.ArgumentParser(description='show csv as heatmap')
 parser.add_argument('--csv')
 parser.add_argument('--sep', default=',')
+parser.add_argument('--save', default=None)
 args = parser.parse_args()
 
 def isNumeric(s):
@@ -31,4 +32,15 @@ arr.flat = [ float(e) if isNumeric(e) else np.nan for e in arr.flat]
 arr = arr.astype(np.float32)
 
 plt.imshow(arr,cmap=cc.cm.gouldian)
+plt.axis("off")
+plt.tight_layout()
+if args.save != None:
+   plt.savefig(args.save,
+        bbox_inches="tight", pad_inches=0
+       )
 plt.show()
+
+#import PIL
+#pil_image = PIL.Image.frombytes('RGB', plt.gcf().canvas.get_width_height(),  plt.gcf().canvas.tostring_rgb())
+#plt.imshow(pil_image)
+#temp_canvas = plt.gcf().canvas.tostring_rgb()
