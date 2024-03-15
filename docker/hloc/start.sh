21e7fd28a1af384b05876916147168c19cc6f08a
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2024-03-15 01:03:05 +0100

    recon ushichka

--- docker/hloc/start.sh
+++ docker/hloc/start.sh
@@ -1,3 +1,1 @@
-podman run --privileged --device nvidia.com/gpu=all -v $(pwd):/workspace:rw -p 8888:8888 --shm-size=8g --rm -it local/hloc jupyter lab --allow-root --no-browser --ip 0.0.0.0 / 
-
-
+podman run --privileged --device nvidia.com/gpu=all -v $(pwd):/workspace:rw -p 8888:8888 --shm-size=8g --rm -it local/hloc jupyter lab --allow-root --no-browser --ip 0.0.0.0 /workspace
